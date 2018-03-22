224178ac6b7cc983282d5dd19dbf5a83a0f9a721
HYOJIN LEE <aqua2chyojin@gmail.com>
2018-03-22 17:18:40 +0900

    .

--- 01_jumptopy/chap04/idol.py
+++ 01_jumptopy/chap04/idol.py
@@ -1,8 +1,19 @@
 f = open(".\\연습생.txt","r", encoding='UTF8')
 candidate_list = f.read()
+# candidate_list = """현아
+# 유라
+# 구하라
+# 하니
+# 손나은
+# 민하
+# 크리스탈
+# 윤아
+# 설리
+# 수지"""
+print(candidate_list)
 f.close()
 
-def show_candidate():
-    str(candidate_list,end='')
+# def show_candidate():
+#     candidate_list
 
-show_candidate()
+# show_candidate(candidate_list)
