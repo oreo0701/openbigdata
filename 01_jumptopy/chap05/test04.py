710d569a6a9c016c0e3d39475cfad69904a0d6de
oreo0701 <oreo0701@gmail.com>
2018-04-10 17:10:13 +0900

    .

--- 01_jumptopy/chap05/test04.py
+++ 01_jumptopy/chap05/test04.py
@@ -1,9 +1,17 @@
 def sum(num1,num2):
-    num1, num2 = input("안녕하세요. 두 수를 입력하세요.(종료: 프로그램 종료): ")
     sum=num1+num2
     return sum
+
+
+num1, num2 = (input("안녕하세요. 두 수를 입력하세요.(종료: 프로그램 종료): ")).split()
+if type(num1) != int:
+    print(f"죄송합니다. 첫번째(또는 두번째) 입력이 {num1}입니다. 숫자를 입력하세요.")
+elif type(num2) != int:
+
 
 try:
     sum()
 except:
+    num1, num2 = (input("안녕하세요. 두 수를 입력하세요.(종료: 프로그램 종료): ")).split()
+
     print(f"죄송합니다. 첫번째(또는 두번째) 입력이 {num1}입니다. 숫자를 입력하세요 '")
